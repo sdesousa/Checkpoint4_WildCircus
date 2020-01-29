4712cad5da08d79ff4c72f59c8fb9b0b2c67933c
sdesousa <sylvaindesousa@free.fr>
2020-01-29 16:33:18 +0100

    price table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ActRepository;
+use App\Repository\PriceRepository;
 use App\Repository\SpectacleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +14,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(SpectacleRepository $spectacleRepository, ActRepository $actRepository): Response
+    public function index(SpectacleRepository $spectacleRepository, ActRepository $actRepository, PriceRepository $priceRepository): Response
     {
         $nextSpectacle = $spectacleRepository->findNextSpectacle();
         $lastAct = $actRepository->findOneBy([], ['created' => 'DESC']);
+        $price = $priceRepository->findOneBy([]);
 
         return $this->render('home/index.html.twig', [
             'nextSpectacle' => $nextSpectacle,
             'lastAct' => $lastAct,
+            'price' => $price,
         ]);
     }
 }
---
+++ src/Entity/Price.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PriceRepository")
+ */
+class Price
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $adult;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $senior;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKid(): ?int
+    {
+        return $this->kid;
+    }
+
+    public function setKid(int $kid): self
+    {
+        $this->kid = $kid;
+
+        return $this;
+    }
+
+    public function getAdult(): ?int
+    {
+        return $this->adult;
+    }
+
+    public function setAdult(int $adult): self
+    {
+        $this->adult = $adult;
+
+        return $this;
+    }
+
+    public function getSenior(): ?int
+    {
+        return $this->senior;
+    }
+
+    public function setSenior(int $senior): self
+    {
+        $this->senior = $senior;
+
+        return $this;
+    }
+}
