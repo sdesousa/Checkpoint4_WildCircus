534af64654031d4978dfc09f154b9903700ae831
sdesousa <sylvaindesousa@free.fr>
2020-01-30 12:16:15 +0100

    booking edit

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\BookingType;
 use App\Repository\BookingRepository;
+use App\Repository\PriceRepository;
+use App\Services\SeatAvailability;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,15 +40,29 @@ class BookingController extends AbstractController
     /**
      * @Route("/{id}/edit", name="admin_booking_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Booking $booking): Response
+    public function edit(Request $request, Booking $booking, PriceRepository $priceRepository, SeatAvailability $seatAvailability): Response
     {
+        $price = $priceRepository->findOneBy([]);
+        $availableSeat = $seatAvailability->availableSeats($booking->getSpectacle());
         $form = $this->createForm(BookingType::class, $booking);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-            $this->addFlash('success', 'Votre réservation a été modifiée');
-            return $this->redirectToRoute('admin_booking_index');
+            $entityManager = $this->getDoctrine()->getManager();
+            $placesKid = $request->request->get('booking')['placesKid'];
+            $placesAdult = $request->request->get('booking')['placesAdult'];
+            $placesSenior = $request->request->get('booking')['placesSenior'];
+            $seats = $placesKid + $placesAdult + $placesSenior;
+            if ($seats <= $availableSeat) {
+                $booking->setNumberTicket($seats);
+                $booking->setTotalPrice($placesKid*$price->getKid()+$placesAdult*$price->getAdult()+$placesSenior*$price->getSenior());
+                $entityManager->persist($booking);
+                $entityManager->flush();
+                $this->addFlash('success', 'Votre réservation a été modifiée');
+                return $this->redirectToRoute('admin_booking_index');
+            } else {
+                $this->addFlash('danger', 'Pas assez de places disponible');
+                return $this->redirectToRoute('admin_booking_edit', ['id' => $booking->getId()]);
+            }
         }
 
         return $this->render('booking/edit.html.twig', [
